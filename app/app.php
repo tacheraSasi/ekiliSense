a89ef74e714e2cc0c6ff8688acf4d131512f1e8a
Tachera Sasi <tacherasasi@gmail.com>
2025-02-23 21:17:01 +0300

    Ellie: create the app class and added the time agi function

---
+++ app/app.php
@@ -0,0 +1,55 @@
+<?php
+
+class App{
+    public static function timeAgo(string $timestamp){
+        $current_time = time();
+        $time_difference = $current_time - $timestamp;
+        $seconds = $time_difference;
+        $minutes = round($seconds / 60);
+        $hours = round($seconds / 3600);
+        $days = round($seconds / 86400);
+        $weeks = round($seconds / 604800);
+        $months = round($seconds / 2629440);
+        $years = round($seconds / 31553280);
+      
+        if ($seconds <= 60) {
+          return 'just now';
+        } elseif ($minutes <= 60) {
+          if ($minutes == 1) {
+            return 'A minute ago';
+          } else {
+            return "$minutes minutes ago";
+          }
+        } elseif ($hours <= 24) {
+          if ($hours == 1) {
+            return 'An hour ago';
+          } else {
+            return "$hours hours ago";
+          }
+        } elseif ($days <= 7) {
+          if ($days == 1) {
+            return 'A day ago';
+          } else {
+            return "$days days ago";
+          }
+        } elseif ($weeks <= 4.3) {
+          if ($weeks == 1) {
+            return 'A week ago';
+          } else {
+            return "$weeks weeks ago";
+          }
+        } elseif ($months <= 12) {
+          if ($months == 1) {
+            return 'A month ago';
+          } else {
+            return "$months months ago";
+          }
+        } else {
+          if ($years == 1) {
+            return 'A year ago';
+          } else {
+            return "$years years ago";
+          }
+        }
+    }
+}
